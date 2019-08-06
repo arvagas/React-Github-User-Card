bce7492cf643f30f77eabda3007eefab58d70376
Arvin Agas <arvagas@gmail.com>
2019-08-06 12:37:45 -0400

    app fetches data then stores in state

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -1,18 +1,27 @@
 import React from 'react'
+import axios from 'axios'
 
-class App extends React.Component() {
+class App extends React.Component {
   constructor() {
     super()
     this.state = {
-      
+      ghUser: []
     }
+  }
+
+  componentDidMount() {
+    const userName = 'arvagas'
+    axios.get(`https://api.github.com/users/${userName}`)
+    .then(res => {
+      this.setState({ghUser: res.data})
+    })
+    .catch(err => console.log('API Error: ', err))
   }
 
   render() {
   
     return (
       <div>
-
       </div>
     )
   }
