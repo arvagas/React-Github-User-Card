debddd1e7d526b50ae1eea04051170822ba1d63a
Arvin Agas <arvagas@gmail.com>
2019-08-06 18:07:25 -0400

    after waiting through multiple rate limits.. search functionality working

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -2,42 +2,70 @@ import React from 'react'
 import axios from 'axios'
 
 import UserCard from './components/UserCard'
+import SearchForm from './components/SearchForm'
+
+import { Box } from '@material-ui/core'
 
 class App extends React.Component {
   constructor() {
     super()
     this.state = {
+      userName: null,
+      searchUser: null,
       ghUser: [],
       ghUserFollowers: []
     }
   }
 
   componentDidMount() {
-    const userName = 'arvagas'
+    const user = this.state.userName
+    if (user !== null) {
+      axios.get(`https://api.github.com/users/${user}`)
+      .then(res => {
+        this.setState({ghUser: res.data})
+      })
+      .catch(err => console.log('API Error: ', err))
 
-    axios.get(`https://api.github.com/users/${userName}`)
-    .then(res => {
-      this.setState({ghUser: res.data})
-    })
-    .catch(err => console.log('API Error: ', err))
+      axios.get(`https://api.github.com/users/${user}/followers`)
+      .then(res => {
+        const followerArr = res.data
+        this.setState({ghUserFollowers: followerArr})
+      })
+      .catch(err => console.log('API Error: ', err))
+    }
+  }
 
-    axios.get(`https://api.github.com/users/${userName}/followers`)
-    .then(res => {
-      const followerArr = res.data
-      this.setState({ghUserFollowers: followerArr})
-    })
-    .catch(err => console.log('API Error: ', err))
+  componentDidUpdate() {
+    if ((this.state.userName !== this.state.searchUser) && (this.state.searchUser !== null)) {
+      this.setState({userName: this.state.searchUser})
+      const user = this.state.searchUser
+
+      axios.get(`https://api.github.com/users/${user}`)
+      .then(res => {
+        this.setState({ghUser: res.data})
+      })
+      .catch(err => console.log('API Error: ', err))
+
+      axios.get(`https://api.github.com/users/${user}/followers`)
+      .then(res => {
+        const followerArr = res.data
+        this.setState({ghUserFollowers: followerArr})
+      })
+      .catch(err => console.log('API Error: ', err))
+    }
+  }
+
+  searchUser = searchName => {
+    this.setState({searchUser: searchName})
   }
 
   render() {
   
     return (
-      <div>
+      <Box display='flex' flexDirection='column'>
+        <SearchForm searchUser={this.searchUser}/>
         <UserCard key={this.state.ghUser.id} ghUser={this.state.ghUser} ghUserFollowers={this.state.ghUserFollowers}/>
-        {/* {this.state.ghUserFollowers.map(follower => (
-          <UserCard key={follower.id} ghUser={follower}/>
-        ))} */}
-      </div>
+      </Box>
     )
   }
 }
---
+++ github-user-card/src/components/SearchForm.js
@@ -0,0 +1,42 @@
+import React from 'react'
+
+import { Box } from '@material-ui/core'
+
+class SearchForm extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            search: ''
+        }
+    }
+
+    handleChanges = event => {
+        this.setState({
+            [event.target.name]:event.target.value
+        })
+    }
+
+    searchUserInfo = event => {
+        event.preventDefault()
+        this.props.searchUser(this.state.search)
+    }
+
+    render() {
+        return (
+            <Box m={2} alignSelf='center'>
+                <form onSubmit={this.searchUserInfo}>
+                    <input
+                        type='text'
+                        placeholder='Search for a user...'
+                        value={this.search}
+                        name='search'
+                        onChange={this.handleChanges}
+                    />
+                    <button type="submit">Search</button>
+                </form>
+            </Box>
+        )
+    }
+}
+
+export default SearchForm
