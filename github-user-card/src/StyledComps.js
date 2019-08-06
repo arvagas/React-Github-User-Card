13526c4923b2647d57acbad58396edc926afc4d9
Arvin Agas <arvagas@gmail.com>
2019-08-06 14:11:56 -0400

    styled user cards

---
+++ github-user-card/src/StyledComps.js
@@ -0,0 +1,10 @@
+import { makeStyles } from '@material-ui/core'
+
+export const userCardStyles = makeStyles({
+    bigAvatar: {
+        margin: 10,
+        width: 125,
+        height: 125,
+        alignSelf: 'center',
+    },
+})
--- github-user-card/src/components/UserCard.js
+++ github-user-card/src/components/UserCard.js
@@ -1,20 +1,24 @@
 import React from 'react'
 
+import { Box, Container, Avatar, Typography, Link } from '@material-ui/core'
+import { userCardStyles } from '../StyledComps'
+
 const UserCard = ({ ghUser }) => {
+    const classes = userCardStyles()
 
     return (
-        <div>
-            <img src={ghUser.avatar_url}/>
-            <div>
-                <h1>{ghUser.name}</h1>
-                <h2>{ghUser.login}</h2>
-                <p>Location: {ghUser.location ? ghUser.location : `None`}</p>
-                <p>Profile: <a href={ghUser.html_url}>{ghUser.html_url}</a></p>
-                <p>Followers: {ghUser.followers}</p>
-                <p>Following: {ghUser.following}</p>
-                <p>Bio: {ghUser.bio ? ghUser.bio : `None`}</p>
-            </div>
-        </div>
+        <Box display='flex' m={3} p={2.4} border={1}>
+            <Avatar src={ghUser.avatar_url} className={classes.bigAvatar}/>
+            <Container>
+                <Typography variant='h5'>{ghUser.name}</Typography>
+                <Typography variant='subtitle1'>{ghUser.login}</Typography>
+                <Typography variant='body2'>Location: {ghUser.location ? ghUser.location : `None`}
+                <br/>Profile: <Link href={ghUser.html_url}>{ghUser.html_url}</Link>
+                <br/>Followers: {ghUser.followers}
+                <br/>Following: {ghUser.following}
+                <br/>Bio: {ghUser.bio ? ghUser.bio : `None`}</Typography>
+            </Container>
+        </Box>
     )
 }
 
